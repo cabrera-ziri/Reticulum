94624018e446f476fbefdd8374476e58a914d248
Ivan Cabrera-Ziri <cabrera@fastmail.com>
2024-01-22 16:03:43 +0100

    poly fit replaced by linear in the fit of sci cont

--- fit_sky_residuals.py
+++ fit_sky_residuals.py
@@ -9,8 +9,12 @@ from glob import glob
 from sklearn.decomposition import PCA
 from joblib import load
 from sklearn.linear_model import HuberRegressor
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, PolynomialFeatures
+from sklearn.pipeline import Pipeline
 from sys import stdin, argv
+
+# set output deirectory here
+odir = '/Users/cabrera-ziri/Work/data/eso/reticulum/sky_residual_corrected4/'
 
 # you can set here the dimensions
 # of the figure
@@ -19,46 +23,60 @@ ysize = 8
 ncol = 3 #no. of columns in legend
 
 # centre the figure on the CaT
-xlim_lo,xlim_hi = 849,870
+xlim_lo,xlim_hi = 849.1,870
 
 # turn this off if you don't want to produce figures
 plot_fig = True
 
 def Huber_continuum(x,y):
- 
-    # standardize    
-    x_scaler, y_scaler = StandardScaler(), StandardScaler()
-    x_train = x_scaler.fit_transform(x[..., None])
-    y_train = y_scaler.fit_transform(y[..., None])
+    #this fits a line to the continuum
+    try:
+        # standardize    
+        x_scaler, y_scaler = StandardScaler(), StandardScaler()
+        x_train = x_scaler.fit_transform(x[..., None])
+        y_train = y_scaler.fit_transform(y[..., None])
 
-    # fit model
-    model = HuberRegressor(epsilon=1)
-    model.fit(x_train, y_train.ravel())
+        # fit model
+        model = HuberRegressor(epsilon=1)
+        model.fit(x_train, y_train.ravel())
 
-    # do some predictions
-    test_x = x
-    predictions = y_scaler.inverse_transform(
-        model.predict(x_scaler.transform(test_x[..., None]))
-        )
-    return(predictions)
+        # do some predictions
+        test_x = x
+        predictions = y_scaler.inverse_transform(
+            model.predict(x_scaler.transform(test_x[..., None]))
+            )
+        return(predictions)
+    except ValueError:
+        print('couldn\'t fit continuum this time! probably too flat already...')
+        return(np.ones_like(y))
 
-def readspec(file):
-	"""
-	this function reads the spectrum from the fits file
-	"""
-	
-	hdu = fits.open(file)
-	
-	# wavelenght0
-	ext=0 
-	crval = hdu[ext].header['crval1']
-	cdelt = hdu[ext].header['cdelt1']
-	naxis = hdu[ext].header['naxis1']
-	        
-	wl = crval+cdelt*np.arange(naxis)
-	flux = hdu[ext].data
-	label='{:}'.format(file[:-5].split('/')[-1])
-	return wl,flux,label  
+def poly_continuum(x,y):
+    # this fits a poly to the continuum
+
+    # assumes wavelength array is in nm
+    # and that fluctuations are in 10nm scale
+    order = int((x[-1]-x[0])/(10))
+
+    try:
+        # standardize    
+        x_scaler, y_scaler = StandardScaler(), StandardScaler()
+        x_train = x_scaler.fit_transform(x[..., None])
+        y_train = y_scaler.fit_transform(y[..., None])
+
+        # fit model
+        model = Pipeline([('poly', PolynomialFeatures(degree=order)),
+                  ('linear', HuberRegressor())])
+
+        model.fit(x_train, y_train.ravel())
+
+        # do some predictions
+        predictions = y_scaler.inverse_transform(
+            model.predict(x_scaler.transform(x[..., None]))
+            )
+        return(predictions)
+    except ValueError:
+        print('couldn\'t fit continuum this time! probably too flat already...')
+        return(np.ones_like(y))    
 
 if __name__ == '__main__':
                 
@@ -67,15 +85,31 @@ if __name__ == '__main__':
         print('reading from stdin...')
         infiles = [i[:-1] for i in stdin.readlines()]
 
-    #here we load pca and sky pixel mask    
-    sky_pixels, pca = load('./sky_residual_pca_16-01-24.joblib')
+    #here we load pca and sky pixel mask
+    pca_file = 'sky_residual_pca_18-01-24v2.joblib'
+    sky_pixels, pca = load(pca_file)
         
-    print('plotting...')
+    print('start reading files...')
 
     for file in infiles:
 
-        wl,flx,label = readspec(file)
-        cnt = Huber_continuum(wl,flx)
+        hdu = fits.open(file)
+
+        #output file
+        ofile = odir+file.split('/')[-1][:-5]+'_srs.fits'
+	
+        # wavelenght0
+        ext=0 
+        crval = hdu[ext].header['crval1']
+        cdelt = hdu[ext].header['cdelt1']
+        naxis = hdu[ext].header['naxis1']
+                
+        wl = crval+cdelt*np.arange(naxis)
+        flx = hdu[ext].data
+        label='{:}'.format(file[:-5].split('/')[-1])
+    
+        # cnt = Huber_continuum(wl,flx)
+        cnt = poly_continuum(wl,flx)
 
         #normalize spectra for pca fit
         norm_flx = flx/cnt
@@ -106,6 +140,7 @@ if __name__ == '__main__':
             ax.plot(wl,_model,'-',lw=1,label='sky residual model')
             ax.plot(wl,flx2-1,ls='-',lw=1,label='sky residual substracted - 1')
 
+            # for reference show restframe the positions of CaT lines
             for _ in [850.04,854.44, 866.45]:
                 ax.axvline(_,lw=1,ls='--',color='k')
 
@@ -114,16 +149,32 @@ if __name__ == '__main__':
                         flx2[(wl>=xlim_lo)&(wl<=xlim_hi)]-1,
                         model[0][(wl[sky_pixels]>=xlim_lo)&(wl[sky_pixels]<=xlim_hi)]]
 
-            _flxs_min,_flxs_max = _flxs.min(),_flxs.max()
+            # _flxs_min,_flxs_max = _flxs.min(),_flxs.max()
+            _flxs_min,_flxs_max = np.percentile(_flxs,2),np.percentile(_flxs,98)
 
             if _flxs_min<0:
-                ax.set_ylim(1.1*_flxs_min,1.1*_flxs_max)
+                # ax.set_ylim(1.1*_flxs_min,1.1*_flxs_max)
+                ax.set_ylim(1.5*_flxs_min-1,1.5*_flxs_max)
             else:
-                ax.set_ylim(0.9*_flxs_min,1.1*_flxs_max)
+                ax.set_ylim(0.5*_flxs_min-1,1.5*_flxs_max)
 
             plt.legend(markerscale=12,ncol=ncol)
             ax.set_xlim(xlim_lo,xlim_hi)
             ax.set_xlabel('wl [nm]')
             ax.set_ylabel('normalized flux')
             plt.tight_layout()
-            plt.show(block=True)
+            # plt.show(block=True)
+            plt.savefig(ofile[:-5]+'.png')
+            plt.close('all')
+
+        # write sky residual corrected file
+        _hdu = fits.PrimaryHDU(corr_flx,header=hdu[0].header)
+
+        # update headear
+        _hdu.header['skypca'] = pca_file 
+    
+        header = _hdu.header       
+        header.comments['skypca'] = 'sky residual pca file used'
+
+        # hdul = fits.HDUList([_hdu])
+        _hdu.writeto(ofile,overwrite=True)
